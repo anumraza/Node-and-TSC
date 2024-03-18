ab5c787c1c157a0ea24cb9e762a57a3c3539a410
Anum Fatima <60568217+anumraza@users.noreply.github.com>
2024-03-18 10:47:22 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstNumber" },
+    { message: "enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operators to prform operation",
+        type: "list",
+        name: "operators",
+        choices: ["Addition", "Subtraction", "Multiplication", "division"],
+    },
+]);
+if (answer.operators === "Addition") {
+    console.log("your answer is ");
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operators === "Subtraction") {
+    console.log("your answer is ");
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operators === "Multiplication") {
+    console.log("your answer is ");
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else {
+    console.log("your answer is ");
+    console.log(answer.firstNumber / answer.secondNumber);
+}
